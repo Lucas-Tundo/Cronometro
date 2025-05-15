2f581b3169d0cfc5487ab9f14806386b50b3ebd7
Lucas Tundo <lucastundo@gmail.com>
2025-05-14 22:44:06 -0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cronometro {
+    internal class  Program {
+        public static void Main(string[] args) {
+            Menu();
+        }
+
+        static void Menu() {
+            Console.Clear();
+            Console.WriteLine("S - Segundo");
+            Console.WriteLine("M - Minuto");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+        }
+
+        static void Start(int time) {
+            int currentTime = 0;
+
+            while (currentTime != time) {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Cronometro finalizado... Voltando para o menu");
+            Thread.Sleep(2500);
+        }
+    }
+}
